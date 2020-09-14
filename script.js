e3259667d7462f0afbe09db03a23a648cab03b80
Xin Yi Li <lixiny0818@gmail.com>
2020-09-13 21:28:37 -0400

    Added homepage, scholarships, opportunities, and app HTML & CSS.

---
+++ script.js
@@ -0,0 +1,20 @@
+// var scholarships = $(".scholarships");
+// scholarships.on("click", changeToScholarships);
+// function changeToScholarships {
+  
+// }
+
+// var opportunities = $(".opportunities");
+// opportunities.on("click", changeToOpportunities);
+// function changeToOpportunities {
+
+// }
+
+// var application process = $(".application process"); application process.on("click", changeToApplicationProcess
+// function changeToApplicationProcess) {
+
+// }
+
+// var home = $(".home"); home.on("click",changeToHome function changeToHome) {
+  
+// }
